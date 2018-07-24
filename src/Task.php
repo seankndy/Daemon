c02a6e06e77a7df2a33a9d604f02882ea3697c4f
Sean Kennedy <sean@kndy.net>
2018-07-23 19:13:46 -0600

    bindTo fix in Task.php

--- src/Task.php
+++ src/Task.php
@@ -39,7 +39,7 @@ class Task {
     
     public function run() {
         $func = $this->func;
-        $func->bindTo($this);
+        $func = $func->bindTo($this);
         $retval = $func();
         return $retval;
     }
